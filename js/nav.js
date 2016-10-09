2512c1e05a8baba4adf747b2ff35e84a89674da3
Eli Morris-Heft <eli.morris.heft@gmail.com>
2016-10-08 21:05:54 -0500

    Create an actual nav bar and slide the page out to reveal it.

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ $(function() {
 
   // Toggle the nav menu when the menu button is clicked
   var siteNav = $('.site-nav');
-  siteNav.on('click', '.menu-button', function() {
+  $('.open-menu-button, .close-menu-button').on('click', function() {
     siteNav.toggleClass('open');
   });
 
