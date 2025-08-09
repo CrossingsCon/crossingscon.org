f94ac293ba62417e153cdc38c53a55dd1f37c751
Bex Fortin <cajunluke@gmail.com>
2025-08-09 18:11:41 -0400

    map dialog looks good now

--- js/zoomymap.js
+++ js/zoomymap.js
@@ -1,32 +1,84 @@
 const zoomables = document.querySelectorAll("img.zoomable");
 
-console.table(zoomables);
-
-const onMapClick = event => {
-  const { target } = event;
-  
-  const dialog = document.createElement("dialog");
-  
-  const closeButton = document.createElement("button");
-  closeButton.setAttribute("autofocus", "true");
-  closeButton.textContent = "❌";
-  closeButton.addEventListener("click", () => {
-    dialog.close();
-    
-    target.parentElement.removeChild(dialog);
-  });
+/**
+ * Builds a structure like this:
+ * 
+ * <div class="mapbox">
+ *   <img src="target source" />
+ *   <figcaption>target's caption</figcaption>
+ * </div>
+ */
+const buildMapbox = (target) => {
+  const parent = target.parentElement;
   
   const image = document.createElement("img");
   image.setAttribute("src", target.src);
   
-  dialog.appendChild(closeButton);
-  dialog.appendChild(image);
+  const caption = document.createElement("figcaption");
+  caption.textContent = parent.querySelector("figcaption").textContent;
   
-  target.parentElement.appendChild(dialog);
+  const mapbox = document.createElement("div");
+  mapbox.setAttribute("class", "mapbox");
+  mapbox.appendChild(image);
+  mapbox.appendChild(caption);
+  
+  return mapbox;
+};
+
+/**
+ * Copies any image (and its caption) into a dialog so it can be seen bigger.
+ */
+const onMapClick = event => {
+  const { target } = event;
+  
+  const container = target.parentElement.parentElement;
+  
+  /*
+   Makes a structure like this:
+   <dialog class="mapdialog">
+     <button autofocus="true">❌</button>
+     <div class="mapbox">
+       <img src="target source" />
+       <figcaption>target's caption</figcaption>
+     </div>
+   </dialog>
+  */
+  
+  const dialog = document.createElement("dialog");
+  dialog.setAttribute("class", "mapdialog");
+  
+  const closeDialog = () => {
+    dialog.close();
+    
+    container.removeChild(dialog);
+  };
+  
+  dialog.addEventListener('click', closeDialog);
+  
+  const contents = document.createElement("div");
+  contents.setAttribute("class", "dialogcontents");
+  contents.addEventListener("click", e => e.stopPropagation());
+  
+  const closeButton = document.createElement("button");
+  closeButton.setAttribute("class", "dialogclose");
+  closeButton.setAttribute("autofocus", "true");
+  closeButton.addEventListener("click", closeDialog);
+  
+  const image = document.createElement("img");
+  image.setAttribute("src", "/images/2025/x.svg");
+  closeButton.appendChild(image);
+  
+  contents.appendChild(closeButton);
+  contents.appendChild(buildMapbox(target));
+  
+  dialog.appendChild(contents);
+  
+  container.appendChild(dialog);
   
   dialog.showModal();
 };
 
+// throw the dialogifier on any image like this
 zoomables.forEach(zoomable => {
   zoomable.addEventListener("click", onMapClick);
 });
